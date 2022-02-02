5ee0fc404d8a2f7d6324158cee9d06ca70cf9abe
adriano25klein@gmail.com <you@example.com>
2022-02-02 19:22:23 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
+import styles from "./home.module.scss";
 import Head from "next/head";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>IgNews</title>
+        <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <span>React</span> world
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90/month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }

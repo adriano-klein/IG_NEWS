8142e9aec81335a81a62102c1edd7e91c0d992e9
adriano25klein@gmail.com <you@example.com>
2022-09-02 08:54:56 -0300

    Teste do ActiveLink

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,39 @@
+import { render } from '@testing-library/react';
+import { ActiveLink } from ".";
+
+jest.mock('next/router', () => {
+  return{
+    useRouter(){
+      return {
+        asPath:"/"
+      }
+    }
+  }
+})
+
+
+//----------------Início do primeiro teste-----------------
+describe('Active link component', () => {
+  it('active link renders correctly', () => {
+  const { getByText } = render(
+    <ActiveLink href="/" activeClassName="active">
+      <a>Home</a>
+    </ActiveLink>
+  )
+  
+  expect(getByText('Home')).toBeInTheDocument()
+})
+//----------------Fim do primeiro teste-------------------
+
+//----------------Início do segundo teste-----------------
+  it('Adds active class if the link as currently active', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    )
+    
+    expect(getByText('Home')).toHaveClass('active')
+  })
+})
+//----------------Fim do segundo teste-----------------

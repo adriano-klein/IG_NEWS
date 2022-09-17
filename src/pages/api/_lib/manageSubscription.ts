30798382322b5fa7bbc2782a02ccdfe738a554e1
adriano25klein@gmail.com <you@example.com>
2022-09-17 13:19:37 -0300

    Downgrade do Next

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -27,6 +27,12 @@ export async function saveSubscription(
     price_id: subscription.items.data[0].price.id,
   };
 
+  try {
+    
+  } catch (error) {
+    
+  }
+
   if (createAction) {
     await fauna.query(
       q.Create(q.Collection("subscriptions"), { data: subscriptionData })
